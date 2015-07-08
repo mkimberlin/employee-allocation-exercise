e5df14f7198adfdfaf534e06c22935a167a4bb6c
Michael Kimberlin <mkimberlin@gmail.com>
2015-07-08 00:38:41 -0500

    Document EmployeeType

--- src/main/java/com/michaelkimberlin/exercises/allocation/model/EmployeeType.java
+++ src/main/java/com/michaelkimberlin/exercises/allocation/model/EmployeeType.java
@@ -1,5 +1,8 @@
 package com.michaelkimberlin.exercises.allocation.model;
 
+/**
+ * Simple enumeration of valid employee types and their associated expense allocations.
+ */
 public enum EmployeeType {
 
 	MANAGER(300),
@@ -8,6 +11,11 @@ public enum EmployeeType {
 
 	private int allocation;
 
+	/**
+	 * Returns the expense allocation allowed for an employee of this type.
+	 * 
+	 * @return the allocation for this employee type in dollars
+	 */
 	public int getAllocation() {
 		return allocation;
 	}
